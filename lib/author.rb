383f69395435374fcdb4f64a698b1e05fc4dbda3
Alla Kuzminova <alchik@gmail.com>
2020-10-22 18:17:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,5 +10,18 @@ class Author
     Post.all.select {|post| post.author == self}
   end
 
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_name(name)
+    post = Post.new(name)
+    post.author = self
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
 
 end
